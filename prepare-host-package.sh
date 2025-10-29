7176e00594e47b3561353c3b24909f48221887ab
alive4ever <alive4ever@users.noreply.github.com>
2025-10-29 14:52:26 +0000

    host: switch back to mmdebstrap
    Undo the debootstrap use and switch back to mmdebstrap.

--- prepare-host-package.sh
+++ prepare-host-package.sh
@@ -1,7 +1,11 @@
 set -e
 umask 022
 sudo apt update
-sudo apt install -y debootstrap systemd-container
+sudo apt install -y mmdebstrap systemd-container
+sudo apt remove -y mmdebstrap
+git clone https://gitlab.mister-muffin.de/josch/mmdebstrap.git
+chmod a+x ./mmdebstrap/mmdebstrap
+sudo ln -s ${PWD}/mmdebstrap/mmdebstrap /usr/local/bin/
 ( cd /var/tmp/ && \
 	curl -sSLO http://raspbian.raspberrypi.com/raspbian/pool/main/r/raspbian-archive-keyring/raspbian-archive-keyring_20120528.4_all.deb && \
 	mkdir -p ./raspbian-keyring && \
@@ -12,6 +16,6 @@ if ! [ -r "$RASPBIAN_KEYRING" ]; then
 echo "Unable to access $RASPBIAN_KEYRING"
 exit 255
 fi
-sudo debootstrap --arch=armhf --keyring="$RASPBIAN_KEYRING" --include sudo,curl,build-essential,devscripts,clang,protobuf-compiler,python3,python3-venv,ninja-build,generate-ninja,cmake,git,nodejs trixie /var/lib/machines/armhf-raspbian http://raspbian.raspberrypi.com/raspbian
+sudo mmdebstrap --arch=armhf --keyring="$RASPBIAN_KEYRING" --include sudo,curl,build-essential,devscripts,clang,protobuf-compiler,python3,python3-venv,ninja-build,generate-ninja,cmake,git,nodejs trixie /var/lib/machines/armhf-raspbian http://raspbian.raspberrypi.com/raspbian
 echo "Container successfully created"
 
