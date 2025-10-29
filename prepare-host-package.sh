a42fdc1d750c804b7e3cfd2e5b5892292b8a85ee
alive4ever <alive4ever@users.noreply.github.com>
2025-10-29 13:51:30 +0000

    host: Add test for keyrings
    Test if the raspberry pi keyring is readable.

--- prepare-host-package.sh
+++ prepare-host-package.sh
@@ -1,4 +1,5 @@
 set -e
+umask 022
 sudo apt update
 sudo apt upgrade -y
 sudo apt install -y mmdebstrap systemd-container debian-archive-keyring
@@ -7,6 +8,10 @@ sudo apt install -y mmdebstrap systemd-container debian-archive-keyring
 	sudo apt install -y ./raspbian-archive-keyring_20120528.4_all.deb && \
 	cd - )
 RASPBIAN_KEYRING="/usr/share/keyrings/raspbian-archive-keyring.gpg"
+if ! [ -r "$RASPBIAN_KEYRING" ]; then
+echo "Unable to access $RASPBIAN_KEYRING"
+exit 255
+fi
 sudo mmdebstrap --arch=armhf --keyring="$RASPBIAN_KEYRING" --include sudo,curl,build-essential,devscripts,clang,protobuf-compiler,python3,python3-venv,ninja-build,generate-ninja,cmake,git,nodejs trixie /var/lib/machines/armhf-raspbian http://raspbian.raspberrypi.com/raspbian
 echo "Container successfully created"
 
