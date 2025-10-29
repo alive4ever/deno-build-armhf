b171729726ebfe93c4bb3ba4a93cbe916183530d
alive4ever <alive4ever@users.noreply.github.com>
2025-10-29 14:18:38 +0000

    host: try to just extract raspbian keyring
    Instead of installing the keyring, extract the raspbian keyring to
    temporary directory.

--- prepare-host-package.sh
+++ prepare-host-package.sh
@@ -5,9 +5,10 @@ sudo apt upgrade -y
 sudo apt install -y mmdebstrap systemd-container debian-archive-keyring
 ( cd /var/tmp/ && \
 	curl -sSLO http://raspbian.raspberrypi.com/raspbian/pool/main/r/raspbian-archive-keyring/raspbian-archive-keyring_20120528.4_all.deb && \
-	sudo apt install -y ./raspbian-archive-keyring_20120528.4_all.deb && \
+	mkdir -p ./raspbian-keyring && \
+	dpkg-deb --extract ./raspbian-archive-keyring_20120528.4_all.deb ./raspbian-keyring && \
 	cd - )
-RASPBIAN_KEYRING="/usr/share/keyrings/raspbian-archive-keyring.gpg"
+RASPBIAN_KEYRING="/var/tmp/raspbian-keyring/usr/share/keyrings/raspbian-archive-keyring.gpg"
 if ! [ -r "$RASPBIAN_KEYRING" ]; then
 echo "Unable to access $RASPBIAN_KEYRING"
 exit 255
