831238e43fbc62edf9cb857a1e5518a21f42def5
alive4ever <alive4ever@users.noreply.github.com>
2025-10-29 09:56:05 +0000

    build: Add build script
    Add actual build script to run the build.

---
+++ build-deno.sh
@@ -0,0 +1,21 @@
+set -e
+umask 022
+DENO_VERSION="v2.5.5"
+PLATFORM="$(cc --dumpmachine)"
+git clone --depth=1 --branch="$DENO_VERSION" https://github.com/denoland/deno
+curl -L -o rustup-install.sh https://sh.rustup.rs
+sh rustup-install.sh -y
+rustc --version
+cargo --version
+cd ./deno
+mkdir -p /tmp/hosttmp/deno_deb
+cargo build --release
+./target/release/deno run tests/testdata/run/002_hello.ts
+TGZNAME="deno-"$DENO_VERSION"-"$PLATFORM".tar.gz"
+tar --numeric-owner -C ./target/release/ -cf - . | gzip -n > /tmp/hosttmp/deno_deb/"$TGZNAME"
+(cd /tmp/hosttmp/deno_deb && sha256sum "$TGZNAME" | tee "$TGZNAME".sha256sum && cd -)
+cargo install cargo-deb
+cargo deb 
+DEBNAME=$(basename $(find ./target/debian -name '*.deb'))
+cp -v ./target/debian/"$DEBNAME" /tmp/hosttmp/deno_deb/
+(cd /tmp/hosttmp/deno_deb && sha256sum "$DEBNAME" | tee "$DEBNAME".sha256sum && cd -)
