01567cc3745e03c41c68695e797b9b98127ab559
alive4ever <alive4ever@users.noreply.github.com>
2025-10-31 02:38:52 +0000

    build: use cross compilation from arm64 debian
    Use cross compilation using arm64 debian chroot.

--- build-deno.sh
+++ build-deno.sh
@@ -4,11 +4,11 @@ cd $HOME
 umask 022
 DENO_VERSION="v2.5.5"
 V8_VERSION="v140.2.0"
-export CC="gcc"
-export CXX="g++"
+PREFIX="arm-linux-gnueabihf"
+PATH="/usr/${PREFIX}/bin:$PATH"
+export CC="$PREFIX-gcc"
+export CXX="$PREFIX-g++"
 PLATFORM="$($CC -dumpmachine)"
-export HOST="arm"
-export TARGET="arm"
 export V8_FROM_SOURCE=1
 export GN="$(command -v gn)"
 export NINJA="$(command -v ninja)"
@@ -17,12 +17,14 @@ CLANG_VERSION=$(clang -dumpversion | cut -d . -f 1)
 export DISABLE_CLANG=1
 export GN_ARGS="target_cpu=\"arm\" v8_target_cpu=\"arm\""
 export PRINT_GN_ARGS=1
+export TARGET="armv6-unknown-linux-gnueabihf"
 
 curl -L -o rustup-install.sh https://sh.rustup.rs
 sh rustup-install.sh -y
 . $HOME/.cargo/env
 rustc --version
 cargo --version
+rustup target add "$TARGET"
 git clone --depth=1 --branch="$V8_VERSION" https://github.com/denoland/rusty_v8
 cd ./rusty_v8
 git config -f .gitmodules submodule.v8.shallow true
@@ -31,13 +33,15 @@ cd -
 git clone --depth=1 --branch="$DENO_VERSION" https://github.com/denoland/deno
 cd ./deno
 mkdir -p /tmp/hosttmp/deno_deb
-cargo build --release
-./target/release/deno run tests/testdata/run/002_hello.ts
+cargo build --target "$TARGET" --release
+echo "Build succeeded at $(date -u)"
+podman run --rm -v ./target/release/deno:/bin/deno arm32v7/busybox deno run tests/testdata/run/002_hello.ts
+echo "Hello test succeeded"
 TGZNAME="deno-"$DENO_VERSION"-"$PLATFORM".tar.gz"
 tar --numeric-owner -C ./target/release/ -cf - . | gzip -n > /tmp/hosttmp/deno_deb/"$TGZNAME"
 (cd /tmp/hosttmp/deno_deb && sha256sum "$TGZNAME" | tee "$TGZNAME".sha256sum && cd -)
 cargo install cargo-deb
-cargo deb 
+cargo deb --target "$TARGET"
 DEBNAME=$(basename $(find ./target/debian -name '*.deb'))
 cp -v ./target/debian/"$DEBNAME" /tmp/hosttmp/deno_deb/
 (cd /tmp/hosttmp/deno_deb && sha256sum "$DEBNAME" | tee "$DEBNAME".sha256sum && cd -)
