f16d98eb15fe519be9898827fa713df1cdb9595a
alive4ever <alive4ever@users.noreply.github.com>
2025-10-31 06:42:59 +0000

    prepare-build: install upstream clang+llvm
    Install upstream clang+llvm before build.

--- prepare-build-chroot.sh
+++ prepare-build-chroot.sh
@@ -1,5 +1,6 @@
 export DEBIAN_FRONTEND="noninteractive"
+echo "deb http://apt.llvm.org/trixie/ llvm-toolchain-trixie-21 main" > /etc/apt/sources.list.d/llvm.list
 apt update
-apt upgrade -y
+apt install -y lld-21 clang-21 clang-tools-21 clang-tidy-21 clang-format-21 libclang-21-dev
 useradd -m -G sudo -s /bin/bash builder
 passwd -d builder
