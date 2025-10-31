cdb4a564255ff2b682444dad83a6fc592fcce509
alive4ever <alive4ever@users.noreply.github.com>
2025-10-31 02:09:22 +0000

    host: use debian arm64 chroot
    Use debian arm64 chroot instead of native armhf.

--- prepare-host-package.sh
+++ prepare-host-package.sh
@@ -1,21 +1,10 @@
 set -e
 umask 022
 sudo apt update
-sudo apt install -y mmdebstrap systemd-container
+sudo apt install -y mmdebstrap systemd-container debian-archive-keyring
 sudo apt remove -y mmdebstrap
 git clone https://gitlab.mister-muffin.de/josch/mmdebstrap.git
 chmod a+x ./mmdebstrap/mmdebstrap
-sudo ln -s ${PWD}/mmdebstrap/mmdebstrap /usr/local/bin/
-( cd /var/tmp/ && \
-	curl -sSLO http://raspbian.raspberrypi.com/raspbian/pool/main/r/raspbian-archive-keyring/raspbian-archive-keyring_20120528.4_all.deb && \
-	mkdir -p ./raspbian-keyring && \
-	dpkg-deb --extract ./raspbian-archive-keyring_20120528.4_all.deb ./raspbian-keyring && \
-	cd - )
-RASPBIAN_KEYRING="/var/tmp/raspbian-keyring/usr/share/keyrings/raspbian-archive-keyring.gpg"
-if ! [ -r "$RASPBIAN_KEYRING" ]; then
-echo "Unable to access $RASPBIAN_KEYRING"
-exit 255
-fi
-sudo mmdebstrap --arch=armhf --keyring="$RASPBIAN_KEYRING" --include sudo,curl,build-essential,devscripts,protobuf-compiler,python3,python3-venv,ninja-build,generate-ninja,cmake,git,nodejs,sccache trixie /var/lib/machines/armhf-raspbian http://raspbian.raspberrypi.com/raspbian
+sudo mmdebstrap --arch=arm64 --include sudo,curl,build-essential,devscripts,protobuf-compiler,python3,python3-venv,ninja-build,generate-ninja,cmake,git,nodejs,sccache trixie /var/lib/machines/arm64-debian http://deb.debian.org/debian
 echo "Container successfully created"
 
