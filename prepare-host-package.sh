d4e9ed02dd5a607cde2822deeaf2e15471cf9acc
alive4ever <alive4ever@users.noreply.github.com>
2025-10-31 09:35:30 +0000

    host: add lsb-release to chroot
    Add lsb-release package.

--- prepare-host-package.sh
+++ prepare-host-package.sh
@@ -2,6 +2,6 @@ set -e
 umask 022
 sudo apt update
 sudo apt install -y mmdebstrap systemd-container debian-archive-keyring
-sudo mmdebstrap --arch=arm64 --include sudo,curl,build-essential,devscripts,protobuf-compiler,python3,python3-venv,ninja-build,generate-ninja,cmake,git,nodejs,sccache,gcc-arm-linux-gnueabihf,podman trixie /var/lib/machines/arm64-debian http://deb.debian.org/debian
+sudo mmdebstrap --arch=arm64 --include sudo,curl,build-essential,devscripts,protobuf-compiler,python3,python3-venv,ninja-build,generate-ninja,cmake,git,nodejs,sccache,gcc-arm-linux-gnueabihf,podman,pkg-config,lsb-release trixie /var/lib/machines/arm64-debian http://deb.debian.org/debian
 echo "Container successfully created"
 
