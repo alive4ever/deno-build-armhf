f24ce28a7b66d88cbeafc747cfd4a2e711f5c6cd
alive4ever <alive4ever@users.noreply.github.com>
2025-10-31 07:59:55 +0000

    build: append clang bin path
    Add clang bin directory to path.

--- build-deno.sh
+++ build-deno.sh
@@ -4,20 +4,17 @@ cd $HOME
 umask 022
 DENO_VERSION="v2.5.5"
 V8_VERSION="v140.2.0"
-CLANG_VERSION="$(clang -dumpversion | cut -d . -f 1)"
+CLANG_VERSION="21"
+export CLANG_BASE_PATH="/usr/lib/llvm-$CLANG_VERSION"
+PATH="$CLANG_BASE_PATH/bin:$PATH"
 PREFIX="arm-linux-gnueabihf"
 export CC="clang"
 export CXX="clang++"
-export LD="$PREFIX-ld"
-export AR="$PREFIX-ar"
-export NM="$PREFIX-nm"
-export RANLIB="$PREFIX-ranlib"
 PLATFORM="$($CC -dumpmachine)"
 export V8_FROM_SOURCE=1
 export GN="$(command -v gn)"
 export NINJA="$(command -v ninja)"
 export SCCACHE="$(command -v sccache)"
-export CLANG_BASE_PATH="/usr/lib/llvm-$CLANG_VERSION"
 export LIBCLANG_PATH="/usr/lib/llvm-$CLANG_VERSION/lib"
 export EXTRA_GN_ARGS="clang_version=\"$CLANG_VERSION\" target_cpu=\"arm\" v8_target_cpu=\"arm\" host_toolchain=\"//build/toolchain/linux/unbundle:default\" custom_toolchain=\"//build/toolchain/linux/unbundle:default\" v8_enable_pointer_compression=\"false\""
 export PRINT_GN_ARGS=1
