dc23e1a709f10ec644fd803bf0c894aee559b5db
alive4ever <alive4ever@users.noreply.github.com>
2025-10-31 02:31:20 +0000

    host: add podman to chroot
    Add podman to chroot.

--- prepare-host-package.sh
+++ prepare-host-package.sh
@@ -5,6 +5,6 @@ sudo apt install -y mmdebstrap systemd-container debian-archive-keyring
 sudo apt remove -y mmdebstrap
 git clone https://gitlab.mister-muffin.de/josch/mmdebstrap.git
 chmod a+x ./mmdebstrap/mmdebstrap
-sudo mmdebstrap --arch=arm64 --include sudo,curl,build-essential,devscripts,protobuf-compiler,python3,python3-venv,ninja-build,generate-ninja,cmake,git,nodejs,sccache trixie /var/lib/machines/arm64-debian http://deb.debian.org/debian
+sudo mmdebstrap --arch=arm64 --include sudo,curl,build-essential,devscripts,protobuf-compiler,python3,python3-venv,ninja-build,generate-ninja,cmake,git,nodejs,sccache,gcc-arm-linux-gnueabihf,podman trixie /var/lib/machines/arm64-debian http://deb.debian.org/debian
 echo "Container successfully created"
 
