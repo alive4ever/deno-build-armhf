4dea9d4168ba50b8f0503e944e5c33ffeac140a7
alive4ever <alive4ever@users.noreply.github.com>
2025-11-01 02:03:57 +0000

    prepare-build: use llvm-20

--- prepare-build-chroot.sh
+++ prepare-build-chroot.sh
@@ -1,8 +1,9 @@
 export DEBIAN_FRONTEND="noninteractive"
-echo "deb http://apt.llvm.org/trixie/ llvm-toolchain-trixie-21 main" > /etc/apt/sources.list.d/llvm.list
+DEBIAN_CODENAME="bookworm"
+echo "deb http://apt.llvm.org/$DEBIAN_CODENAME/ llvm-toolchain-trixie-21 main" > /etc/apt/sources.list.d/llvm.list
 curl -sSL https://apt.llvm.org/llvm-snapshot.gpg.key | sudo tee /etc/apt/trusted.gpg.d/apt.llvm.org.asc
 apt-get update
-apt-get install -y lld-21 clang-21 clang-tools-21 clang-tidy-21 clang-format-21 libclang-21-dev
+apt-get install -y lld-20 clang-20 clang-tools-20 clang-tidy-20 clang-format-20 libclang-20-dev
 apt-get install -y $(</tmp/hosttmp/packages.txt)
 useradd -m -G sudo -s /bin/bash builder
 passwd -d builder
