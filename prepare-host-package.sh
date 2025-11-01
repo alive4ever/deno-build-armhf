e36478a6fc404170df888c34d0b6738500d060c6
alive4ever <alive4ever@users.noreply.github.com>
2025-11-01 02:03:09 +0000

    host: switch to bookworm
    Switch the bootstrapped distro to bookworm.

--- prepare-host-package.sh
+++ prepare-host-package.sh
@@ -1,7 +1,8 @@
 set -e
 umask 022
+DEBIAN_CODENAME="bookworm"
 sudo apt update
 sudo apt install -y mmdebstrap systemd-container debian-archive-keyring
-sudo mmdebstrap --arch=arm64 --include sudo,curl,build-essential,devscripts,protobuf-compiler,python3,python3-venv,ninja-build,generate-ninja,cmake,git,nodejs,sccache,gcc-arm-linux-gnueabihf,podman,pkg-config,lsb-release trixie /var/lib/machines/arm64-debian http://deb.debian.org/debian
+sudo mmdebstrap --arch=arm64 --include sudo,curl,build-essential,devscripts,protobuf-compiler,python3,python3-venv,ninja-build,generate-ninja,cmake,git,nodejs,sccache,gcc-arm-linux-gnueabihf,podman,pkg-config,lsb-release "$DEBIAN_CODENAME" /var/lib/machines/arm64-debian http://deb.debian.org/debian
 echo "Container successfully created"
 
