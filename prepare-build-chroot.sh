487a7f0649603cf315aba332672434dad4e7145d
alive4ever <alive4ever@users.noreply.github.com>
2025-11-01 02:12:24 +0000

    prepare-build: fix llvm toolchain repo name
    Fix llvm toolchain repo name.

--- prepare-build-chroot.sh
+++ prepare-build-chroot.sh
@@ -1,6 +1,6 @@
 export DEBIAN_FRONTEND="noninteractive"
 DEBIAN_CODENAME="bookworm"
-echo "deb http://apt.llvm.org/$DEBIAN_CODENAME/ llvm-toolchain-trixie-21 main" > /etc/apt/sources.list.d/llvm.list
+echo "deb http://apt.llvm.org/$DEBIAN_CODENAME/ llvm-toolchain-$DEBIAN_CODENAME-20 main" > /etc/apt/sources.list.d/llvm.list
 curl -sSL https://apt.llvm.org/llvm-snapshot.gpg.key | sudo tee /etc/apt/trusted.gpg.d/apt.llvm.org.asc
 apt-get update
 apt-get install -y lld-20 clang-20 clang-tools-20 clang-tidy-20 clang-format-20 libclang-20-dev
