8b1e5a42d9df5a8a35ce1472cdc77dc61878ff1d
alive4ever <alive4ever@users.noreply.github.com>
2025-11-04 03:02:45 +0000

    prepare-build: reinstall chroot packages

--- prepare-build-chroot.sh
+++ prepare-build-chroot.sh
@@ -2,5 +2,6 @@ export DEBIAN_FRONTEND="noninteractive"
 DEBIAN_CODENAME="bookworm"
 apt-get update
 apt-get build-dep -y chromium
+apt-get install -y $(cat /tmp/hosttmp/chroot-packages.txt | tr \, \\n)
 useradd -m -G sudo -s /bin/bash builder
 passwd -d builder
