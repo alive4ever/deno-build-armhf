5955b7a143af7e5bdd0ff24017dbe83f1a678793
alive4ever <alive4ever@users.noreply.github.com>
2025-11-04 02:18:52 +0000

    host: fallback to sources.list

--- prepare-host-package.sh
+++ prepare-host-package.sh
@@ -4,18 +4,12 @@ DEBIAN_CODENAME="bookworm"
 sudo apt update
 sudo apt install -y mmdebstrap systemd-container debian-archive-keyring
 cat << EOL > /tmp/debian.sources
-Types: deb deb-src
-URIs: https://deb.debian.org/debian/
-Suites: $DEBIAN_CODENAME $DEBIAN_CODENAME-updates
-Components: main contrib
-Signed-By: /usr/share/keyrings/debian-archive-keyring.gpg
-
-Types: deb deb-src
-URIs: https://security.debian.org/debian-security/
-Suites: $DEBIAN_CODENAME-security
-Components: main contrib
-Enabled: yes
-Signed-By: /usr/share/keyrings/debian-archive-keyring.gpg
+deb https://deb.debian.org/debian/ $DEBIAN_CODENAME main contrib
+deb-src https://deb.debian.org/debian/ $DEBIAN_CODENAME main contrib
+deb https://deb.debian.org/debian/ $DEBIAN_CODENAME-updates main contrib
+deb-src https://deb.debian.org/debian/ $DEBIAN_CODENAME-updates main contrib
+deb https://security.debian.org/debian-security/ $DEBIAN_CODENAME-security main contrib
+deb-src https://security.debian.org/debian-security/ $DEBIAN_CODENAME-security main contrib
 EOL
 cat /tmp/debian.sources | sudo mmdebstrap --keyring=/usr/share/keyrings --arch=arm64 --include "$(</tmp/chroot-packages.txt)" "$DEBIAN_CODENAME" /var/lib/machines/arm64-debian -
 echo "Container successfully created"
