e7f264c15e6f0096ac0e4dc3b8ee4e928ccb46ab
alive4ever <alive4ever@users.noreply.github.com>
2026-08-15 06:28:29 +0000

    prepare: add signed-by to llvm.list

--- prepare-build-chroot.sh
+++ prepare-build-chroot.sh
@@ -1,11 +1,12 @@
+set -e
 export DEBIAN_FRONTEND="noninteractive"
 DEBIAN_CODENAME="bookworm"
+umask 022
 curl -L https://apt.llvm.org/llvm-snapshot.gpg.key | tee /etc/apt/trusted.gpg.d/apt.llvm.org.asc
-echo 'deb http://apt.llvm.org/bookworm/ llvm-toolchain-bookworm-21 main
-deb-src http://apt.llvm.org/bookworm/ llvm-toolchain-bookworm-21 main
+echo 'deb [signed-by=/etc/apt/trusted.gpg.d/apt.llvm.org.asc] https://apt.llvm.org/bookworm/ llvm-toolchain-bookworm-21 main
+deb-src [signed-by=/etc/apt/trusted.gpg.d/apt.llvm.org.asc] https://apt.llvm.org/bookworm/ llvm-toolchain-bookworm-21 main
 ' | tee /etc/apt/sources.list.d/llvm.list
 apt-get update
-apt-get install -y $(cat /tmp/hosttmp/chroot-packages.txt | tr \, \\n)
 apt-get install -y clang-21 lld-21 lldb-21
 useradd -m -G sudo -s /bin/bash builder
 passwd -d builder
