31a34bded184cdc956e509bfa969f9ca89df4a53
alive4ever <alive4ever@users.noreply.github.com>
2026-08-15 07:06:28 +0000

    chroot: switch to trixie

--- prepare-build-chroot.sh
+++ prepare-build-chroot.sh
@@ -1,10 +1,10 @@
 set -e
 export DEBIAN_FRONTEND="noninteractive"
-DEBIAN_CODENAME="bookworm"
+DEBIAN_CODENAME="trixie"
 umask 022
 curl -L https://apt.llvm.org/llvm-snapshot.gpg.key | tee /etc/apt/trusted.gpg.d/apt.llvm.org.asc
-echo 'deb [signed-by=/etc/apt/trusted.gpg.d/apt.llvm.org.asc] https://apt.llvm.org/bookworm/ llvm-toolchain-bookworm-21 main
-deb-src [signed-by=/etc/apt/trusted.gpg.d/apt.llvm.org.asc] https://apt.llvm.org/bookworm/ llvm-toolchain-bookworm-21 main
+echo 'deb [signed-by=/etc/apt/trusted.gpg.d/apt.llvm.org.asc] https://apt.llvm.org/trixie/ llvm-toolchain-trixie-21 main
+deb-src [signed-by=/etc/apt/trusted.gpg.d/apt.llvm.org.asc] https://apt.llvm.org/trixie/ llvm-toolchain-trixie-21 main
 ' | tee /etc/apt/sources.list.d/llvm.list
 apt-get update
 apt-get install -y $(cat /tmp/hosttmp/chroot-packages.txt | tr \, \\n)
