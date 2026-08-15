439455a981cf215732e31997c112eccd4addf89c
alive4ever <alive4ever@users.noreply.github.com>
2026-08-15 06:32:21 +0000

    prepare-build: install chroot packages

--- prepare-build-chroot.sh
+++ prepare-build-chroot.sh
@@ -7,6 +7,7 @@ echo 'deb [signed-by=/etc/apt/trusted.gpg.d/apt.llvm.org.asc] https://apt.llvm.o
 deb-src [signed-by=/etc/apt/trusted.gpg.d/apt.llvm.org.asc] https://apt.llvm.org/bookworm/ llvm-toolchain-bookworm-21 main
 ' | tee /etc/apt/sources.list.d/llvm.list
 apt-get update
+apt-get install -y $(cat /tmp/hosttmp/chroot-packages.txt | tr \, \\n)
 apt-get install -y clang-21 lld-21 lldb-21
 useradd -m -G sudo -s /bin/bash builder
 passwd -d builder
