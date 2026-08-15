c52aea761977402e69cbe0176d7543fbe5911a0f
alive4ever <alive4ever@users.noreply.github.com>
2026-08-15 12:56:20 +0000

    build: fix linker env

--- build-deno.sh
+++ build-deno.sh
@@ -25,8 +25,10 @@ RUST_TARGET="armv7-unknown-linux-gnueabihf"
 export CC_armv7_unknown_linux_gnueabihf="$PREFIX-gcc"
 export CXX_armv7_unknown_linux_gnueabihf="$PREFIX-g++"
 export AR_armv7_unknown_linux_gnueabihf="$PREFIX-ar"
-export CARGO_TARGET_ARMV7_UNKNOWN_LINUX_GNUEABIHF="$PREFIX-ld"
-
+export CARGO_TARGET_ARMV7_UNKNOWN_LINUX_GNUEABIHF_LINKER="$PREFIX-gcc"
+export QEMU_LD_PREFIX="/usr/$PREFIX"
+sudo ln -s /usr/$PREFIX/lib/ld-linux-armhf.so.3 \
+	/lib/ld-linux-armhf.so.3 || echo "ld-linux-armhf is already linked"
 curl -L -o rustup-install.sh https://sh.rustup.rs
 sh rustup-install.sh -y -t "$RUST_TARGET" --default-toolchain 1.95.0
 . $HOME/.cargo/env
